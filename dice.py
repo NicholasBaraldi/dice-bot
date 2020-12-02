8693db98a1fad12a37972240574d7759b384dc37
Nicholas Baraldi da Silva Souza <nicholas.baraldi@gamil.com>
2020-12-02 16:50:34 -0300

    refactoring main logic

--- dice.py
+++ dice.py
@@ -1,8 +1,12 @@
 import discord
 import random
 import json
+import re
 
 client = discord.Client()
+
+dice_regex = re.compile(r'!(\d+)?D(\d+)')
+dice_num_regex = re.compile(r'!(\d+)D')
 
 @client.event
 async def on_ready():
@@ -13,29 +17,28 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!'):
-        m = message.content[1]
-        if m == 'D':
-            dice = int(message.content.split('D')[-1])
-            author = message.author.mention
-            if dice > 100:
-                await message.channel.send(str(author) + " escolha um numero entre 1 e 100")
-            else:
-                msg = "{} tirou {}".format(author, random.randint(1, dice))
-                await message.channel.send(msg)
-        elif m.isnumeric():
-            m = int(message.content[1])
-            i = 0
-            dice = int(message.content.split('D')[-1])
-            author = message.author.mention
-            if dice > 100:
-                await message.channel.send(str(author) + " escolha um numero entre 1 e 100")
-            else:
-                msg = "{} tirou {}".format(author, random.randint(1, dice))
-                while i in range(m-1):
-                    msg += ", {}".format(random.randint(1, dice))
-                    i += 1
-                await message.channel.send(msg)
+    author = message.author.mention
+
+    if dice_regex.search(message.content):
+        match = dice_num_regex.search(message.content)
+        if match == None:
+            m = 1
+        else:
+            m_start = match.start()
+            m_end = match.end()
+            m = int(message.content[m_start+1: m_end-1])
+
+        dice = int(message.content.split('D')[-1])
+
+        if m > 100 or m == 0:
+            await message.channel.send(str(author) + " escolha um numero de dados entre 1 e 100")
+        elif dice > 100:
+            await message.channel.send(str(author) + " escolha um numero de faces entre 1 e 100")
+        else:
+            msg = "{} tirou {}".format(author, random.randint(1, dice))
+            for i in range(1, m):
+                msg += ", {}".format(random.randint(1, dice))
+            await message.channel.send(msg)
 
 with open(r"token.json", "r") as json_file:
     secret = json.load(json_file)
